9c6e4cdeb94a6037454dd653131084ea71efc911
Dima Sbitnev <helmut3d@mail.ru>
2025-09-24 11:19:15 +0300

    Критичные исправления

--- tests/test_logout.py
+++ tests/test_logout.py
@@ -1,6 +1,7 @@
 import pytest
 from pages.main_page import MainPage
 from pages.login_page import LoginPage
+from pages.registration_page import RegistrationPage
 from pages.profile_page import ProfilePage
 from utils.generators import generate_email, generate_password, generate_name
 
@@ -29,10 +30,3 @@ class TestLogout:
 
         login_page = LoginPage(driver)
         assert login_page.is_login_page_opened()
-        main_page.click_personal_account_button()
-
-        profile_page = ProfilePage(driver)
-        profile_page.click_logout_button()
-
-        login_page = LoginPage(driver)
-        assert login_page.is_login_page()
