2bb2c2b590e9c95f1bb8fa7de292fc165940bb06
Dima Sbitnev <helmut3d@mail.ru>
2025-09-24 11:16:48 +0300

    Критичные исправления

--- pages/login_page.py
+++ pages/login_page.py
@@ -5,15 +5,7 @@ class LoginPage(BasePage):
     def __init__(self, driver):
         super().__init__(driver)
 
-    def is_login_page_opened(self):
+    def is_login_page_opened(self):  # Переименован для единообразия
+
         self.wait_for_url_to_contain("/login")
         return "/login" in self.get_current_url()
-
-    def click_register_link(self):
-        pass
-
-    def click_forgot_password_link(self):
-        pass
-
-    def login(self, email, password):
-        pass
